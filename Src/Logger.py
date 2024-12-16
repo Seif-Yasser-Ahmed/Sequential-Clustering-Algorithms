13982f05188d4d9ab31d77e9a5b3d40d64366d03
Seif Yasser <21p0102@eng.asu.edu.eg>
2024-12-17 01:36:04 +0200

    add .gitignore, output JSON files for clusters, and update Logger for plotting and saving

--- Src/Logger.py
+++ Src/Logger.py
@@ -13,18 +13,30 @@ class Logger:
             json.dump(data, file, cls=NumpyEncoder)
 
     def PlotClusters(data, clusters, method):
-        for cluster_id, points in clusters.items():
+        plt.figure(figsize=(10, 6))
+        means = []
+        for cluster in clusters:
+            cluster_id = cluster['Cluster']
+            points = cluster['Members']
+            mean = cluster['Mean']
+            means.append(mean)
             points = np.array(points)
             plt.scatter(points[:, 0], points[:, 1],
                         label=f'Cluster {cluster_id}')
+            plt.scatter(mean[0], mean[1], marker='x', color='red')
+            plt.text(mean[0], mean[1], f'Mean {
+                     cluster_id}', fontsize=9, color='red')
 
         plt.title(f'Clusters using {method}')
         plt.xlabel('Feature 1')
         plt.ylabel('Feature 2')
+        for i, mean in enumerate(means):
+            plt.scatter([], [], marker='x', color='red', label=f'Mean {
+                        i+1}: ({mean[0]:.2f}, {mean[1]:.2f})')
         plt.legend()
-        plt.savefig(f'clusters_{method}.png')
+        plt.savefig(f'Output/clusters_{method}.png')
 
     @staticmethod
     def log_clusters(data, clusters, distance_method='euclidean'):
-        Logger.save_json(clusters, f'clusters_{distance_method}.json')
+        Logger.save_json(clusters, f'Output/clusters_{distance_method}.json')
         Logger.PlotClusters(data, clusters, distance_method)
