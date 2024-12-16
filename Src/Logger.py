4e519423fc7f85c9c68c38f548ec19c09f6b4993
Seif Yasser <21p0102@eng.asu.edu.eg>
2024-12-17 01:17:56 +0200

    add clustering visualization and update logging functionality in Logger

--- Src/Logger.py
+++ Src/Logger.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from Src.BSAS import BSAS
 from Src.NumpyEncoder import NumpyEncoder
+import matplotlib.pyplot as plt
 
 
 class Logger:
@@ -11,9 +12,19 @@ class Logger:
         with open(file_name, 'w') as file:
             json.dump(data, file, cls=NumpyEncoder)
 
+    def PlotClusters(data, clusters, method):
+        for cluster_id, points in clusters.items():
+            points = np.array(points)
+            plt.scatter(points[:, 0], points[:, 1],
+                        label=f'Cluster {cluster_id}')
+
+        plt.title(f'Clusters using {method}')
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend()
+        plt.savefig(f'clusters_{method}.png')
+
     @staticmethod
-    def log_clusters(data, max_no_clusters, threshold, distance_methods):
-        for method in distance_methods:
-            bsas = BSAS(max_no_clusters, threshold, method)
-            clusters = bsas.fit(data)
-            Logger.save_json(clusters, f'clusters_{method}.json')
+    def log_clusters(data, clusters, distance_method='euclidean'):
+        Logger.save_json(clusters, f'clusters_{distance_method}.json')
+        Logger.PlotClusters(data, clusters, distance_method)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from Src.BSAS import BSAS
 from Src.NumpyEncoder import NumpyEncoder
+from Src.Logger import Logger
 import json
 import numpy as np
 
 
 if __name__ == "__main__":
-    pass
+    data = np.array([[2, 3], [5, 4], [9, 2], [2, 5], [1, 4], [6, 4], [5, 3], [2, 2], [3, 3], [8, 2], [2, 4], [10, 2], [11, 2], [10, 3], [9, 1]
+                     ])
+    max_no_clusters = 15
+    threshold = 2.5
+    distance_methods = ['euclidean', 'manhattan']
+    for distance_method in distance_methods:
+        bsas = BSAS(max_no_clusters=max_no_clusters,
+                    threshold=threshold, distance_method=distance_method)
+        clusters = bsas.fit(data)
+        Logger.log_clusters(data, clusters, distance_method)
