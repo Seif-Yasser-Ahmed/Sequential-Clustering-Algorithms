efa58ba0fb97dc8433248734c49e4d6bc813f8c6
Seif Yasser <21p0102@eng.asu.edu.eg>
2024-12-17 01:01:27 +0200

    add BSAS class and main entry point; create Logger module

---
+++ Src/BSAS.py
@@ -0,0 +1,8 @@
+from .NumpyEncoder import NumpyEncoder
+from .distances import DistanceCalculator
+import json
+import numpy as np
+
+
+class BSAS:
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from Src.BSAS import BSAS
+from Src.NumpyEncoder import NumpyEncoder
+import json
+import numpy as np
+
+
+if __name__ == "__main__":
+    pass
